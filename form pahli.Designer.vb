5d6fe59952d402495f65cb5a53c7dd4654763a15
gingko098 <fjummadil@gmail.com>
2022-01-29 15:41:41 +0700

    first commit

---
+++ form pahli.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtlayar = New System.Windows.Forms.TextBox()
+        Me.cmbbutton1 = New System.Windows.Forms.Button()
+        Me.cmbbutton2 = New System.Windows.Forms.Button()
+        Me.cmbbutton3 = New System.Windows.Forms.Button()
+        Me.cmbbutton4 = New System.Windows.Forms.Button()
+        Me.cmbbutton5 = New System.Windows.Forms.Button()
+        Me.cmbbutton6 = New System.Windows.Forms.Button()
+        Me.cmbbutton7 = New System.Windows.Forms.Button()
+        Me.cmbbutton8 = New System.Windows.Forms.Button()
+        Me.cmbbutton9 = New System.Windows.Forms.Button()
+        Me.cmbbutton0 = New System.Windows.Forms.Button()
+        Me.cmbtambah = New System.Windows.Forms.Button()
+        Me.cmbkurang = New System.Windows.Forms.Button()
+        Me.cmbkali = New System.Windows.Forms.Button()
+        Me.cmbbagi = New System.Windows.Forms.Button()
+        Me.cmbhapus = New System.Windows.Forms.Button()
+        Me.cmbsamadengan = New System.Windows.Forms.Button()
+        Me.cmbpangkat = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtlayar
+        '
+        Me.txtlayar.Location = New System.Drawing.Point(53, 79)
+        Me.txtlayar.Name = "txtlayar"
+        Me.txtlayar.Size = New System.Drawing.Size(398, 20)
+        Me.txtlayar.TabIndex = 0
+        '
+        'cmbbutton1
+        '
+        Me.cmbbutton1.Location = New System.Drawing.Point(53, 262)
+        Me.cmbbutton1.Name = "cmbbutton1"
+        Me.cmbbutton1.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton1.TabIndex = 1
+        Me.cmbbutton1.Text = "1"
+        Me.cmbbutton1.UseVisualStyleBackColor = True
+        '
+        'cmbbutton2
+        '
+        Me.cmbbutton2.Location = New System.Drawing.Point(151, 262)
+        Me.cmbbutton2.Name = "cmbbutton2"
+        Me.cmbbutton2.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton2.TabIndex = 2
+        Me.cmbbutton2.Text = "2"
+        Me.cmbbutton2.UseVisualStyleBackColor = True
+        '
+        'cmbbutton3
+        '
+        Me.cmbbutton3.Location = New System.Drawing.Point(249, 262)
+        Me.cmbbutton3.Name = "cmbbutton3"
+        Me.cmbbutton3.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton3.TabIndex = 3
+        Me.cmbbutton3.Text = "3"
+        Me.cmbbutton3.UseVisualStyleBackColor = True
+        '
+        'cmbbutton4
+        '
+        Me.cmbbutton4.Location = New System.Drawing.Point(53, 224)
+        Me.cmbbutton4.Name = "cmbbutton4"
+        Me.cmbbutton4.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton4.TabIndex = 4
+        Me.cmbbutton4.Text = "4"
+        Me.cmbbutton4.UseVisualStyleBackColor = True
+        '
+        'cmbbutton5
+        '
+        Me.cmbbutton5.Location = New System.Drawing.Point(151, 224)
+        Me.cmbbutton5.Name = "cmbbutton5"
+        Me.cmbbutton5.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton5.TabIndex = 5
+        Me.cmbbutton5.Text = "5"
+        Me.cmbbutton5.UseVisualStyleBackColor = True
+        '
+        'cmbbutton6
+        '
+        Me.cmbbutton6.Location = New System.Drawing.Point(249, 224)
+        Me.cmbbutton6.Name = "cmbbutton6"
+        Me.cmbbutton6.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton6.TabIndex = 6
+        Me.cmbbutton6.Text = "6"
+        Me.cmbbutton6.UseVisualStyleBackColor = True
+        '
+        'cmbbutton7
+        '
+        Me.cmbbutton7.Location = New System.Drawing.Point(53, 186)
+        Me.cmbbutton7.Name = "cmbbutton7"
+        Me.cmbbutton7.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton7.TabIndex = 7
+        Me.cmbbutton7.Text = "7"
+        Me.cmbbutton7.UseVisualStyleBackColor = True
+        '
+        'cmbbutton8
+        '
+        Me.cmbbutton8.Location = New System.Drawing.Point(151, 186)
+        Me.cmbbutton8.Name = "cmbbutton8"
+        Me.cmbbutton8.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton8.TabIndex = 8
+        Me.cmbbutton8.Text = "8"
+        Me.cmbbutton8.UseVisualStyleBackColor = True
+        '
+        'cmbbutton9
+        '
+        Me.cmbbutton9.Location = New System.Drawing.Point(249, 186)
+        Me.cmbbutton9.Name = "cmbbutton9"
+        Me.cmbbutton9.Size = New System.Drawing.Size(92, 32)
+        Me.cmbbutton9.TabIndex = 9
+        Me.cmbbutton9.Text = "9"
+        Me.cmbbutton9.UseVisualStyleBackColor = True
+        '
+        'cmbbutton0
+        '
+        Me.cmbbutton0.Location = New System.Drawing.Point(53, 300)
+        Me.cmbbutton0.Name = "cmbbutton0"
+        Me.cmbbutton0.Size = New System.Drawing.Size(288, 32)
+        Me.cmbbutton0.TabIndex = 10
+        Me.cmbbutton0.Text = "0"
+        Me.cmbbutton0.UseVisualStyleBackColor = True
+        '
+        'cmbtambah
+        '
+        Me.cmbtambah.Location = New System.Drawing.Point(53, 152)
+        Me.cmbtambah.Name = "cmbtambah"
+        Me.cmbtambah.Size = New System.Drawing.Size(72, 28)
+        Me.cmbtambah.TabIndex = 11
+        Me.cmbtambah.Text = "+"
+        Me.cmbtambah.UseVisualStyleBackColor = True
+        '
+        'cmbkurang
+        '
+        Me.cmbkurang.Location = New System.Drawing.Point(151, 152)
+        Me.cmbkurang.Name = "cmbkurang"
+        Me.cmbkurang.Size = New System.Drawing.Size(72, 28)
+        Me.cmbkurang.TabIndex = 12
+        Me.cmbkurang.Text = "-"
+        Me.cmbkurang.UseVisualStyleBackColor = True
+        '
+        'cmbkali
+        '
+        Me.cmbkali.Location = New System.Drawing.Point(249, 152)
+        Me.cmbkali.Name = "cmbkali"
+        Me.cmbkali.Size = New System.Drawing.Size(72, 28)
+        Me.cmbkali.TabIndex = 13
+        Me.cmbkali.Text = "X"
+        Me.cmbkali.UseVisualStyleBackColor = True
+        '
+        'cmbbagi
+        '
+        Me.cmbbagi.Location = New System.Drawing.Point(347, 152)
+        Me.cmbbagi.Name = "cmbbagi"
+        Me.cmbbagi.Size = New System.Drawing.Size(52, 28)
+        Me.cmbbagi.TabIndex = 14
+        Me.cmbbagi.Text = "/"
+        Me.cmbbagi.UseVisualStyleBackColor = True
+        '
+        'cmbhapus
+        '
+        Me.cmbhapus.Location = New System.Drawing.Point(347, 186)
+        Me.cmbhapus.Name = "cmbhapus"
+        Me.cmbhapus.Size = New System.Drawing.Size(104, 45)
+        Me.cmbhapus.TabIndex = 15
+        Me.cmbhapus.Text = "C"
+        Me.cmbhapus.UseVisualStyleBackColor = True
+        '
+        'cmbsamadengan
+        '
+        Me.cmbsamadengan.Location = New System.Drawing.Point(347, 237)
+        Me.cmbsamadengan.Name = "cmbsamadengan"
+        Me.cmbsamadengan.Size = New System.Drawing.Size(104, 95)
+        Me.cmbsamadengan.TabIndex = 16
+        Me.cmbsamadengan.Text = "="
+        Me.cmbsamadengan.UseVisualStyleBackColor = True
+        '
+        'cmbpangkat
+        '
+        Me.cmbpangkat.Location = New System.Drawing.Point(405, 152)
+        Me.cmbpangkat.Name = "cmbpangkat"
+        Me.cmbpangkat.Size = New System.Drawing.Size(46, 28)
+        Me.cmbpangkat.TabIndex = 17
+        Me.cmbpangkat.Text = "^"
+        Me.cmbpangkat.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(415, 348)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(36, 32)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "off"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(506, 392)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmbpangkat)
+        Me.Controls.Add(Me.cmbsamadengan)
+        Me.Controls.Add(Me.cmbhapus)
+        Me.Controls.Add(Me.cmbbagi)
+        Me.Controls.Add(Me.cmbkali)
+        Me.Controls.Add(Me.cmbkurang)
+        Me.Controls.Add(Me.cmbtambah)
+        Me.Controls.Add(Me.cmbbutton0)
+        Me.Controls.Add(Me.cmbbutton9)
+        Me.Controls.Add(Me.cmbbutton8)
+        Me.Controls.Add(Me.cmbbutton7)
+        Me.Controls.Add(Me.cmbbutton6)
+        Me.Controls.Add(Me.cmbbutton5)
+        Me.Controls.Add(Me.cmbbutton4)
+        Me.Controls.Add(Me.cmbbutton3)
+        Me.Controls.Add(Me.cmbbutton2)
+        Me.Controls.Add(Me.cmbbutton1)
+        Me.Controls.Add(Me.txtlayar)
+        Me.Name = "calculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtlayar As System.Windows.Forms.TextBox
+    Friend WithEvents cmbbutton1 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton2 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton3 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton4 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton5 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton6 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton7 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton8 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton9 As System.Windows.Forms.Button
+    Friend WithEvents cmbbutton0 As System.Windows.Forms.Button
+    Friend WithEvents cmbtambah As System.Windows.Forms.Button
+    Friend WithEvents cmbkurang As System.Windows.Forms.Button
+    Friend WithEvents cmbkali As System.Windows.Forms.Button
+    Friend WithEvents cmbbagi As System.Windows.Forms.Button
+    Friend WithEvents cmbhapus As System.Windows.Forms.Button
+    Friend WithEvents cmbsamadengan As System.Windows.Forms.Button
+    Friend WithEvents cmbpangkat As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ form pahli.vb
@@ -0,0 +1,160 @@
+﻿Public Class calculator
+    Friend Hasil As Single = 0
+    Dim operasi As String = ""
+    Dim operasiHitung As Boolean = False
+
+    Private Sub cmbbutton0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton0.Click
+        txtlayar.Text = txtlayar.Text & "0"
+    End Sub
+
+    Private Sub cmbbutton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton1.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "1"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "1"
+        End If
+    End Sub
+
+    Private Sub cmbbutton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton2.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "2"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "2"
+        End If
+    End Sub
+
+    Private Sub cmbbutton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton3.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "3"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "3"
+        End If
+    End Sub
+
+    Private Sub cmbbutton4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton4.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "4"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "4"
+        End If
+    End Sub
+
+    Private Sub cmbbutton5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton5.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "5"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "5"
+        End If
+    End Sub
+
+    Private Sub cmbbutton6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton6.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "6"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "6"
+        End If
+    End Sub
+
+    Private Sub cmbbutton7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton7.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "7"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "7"
+        End If
+    End Sub
+
+    Private Sub cmbbutton8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton8.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "8"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "8"
+        End If
+    End Sub
+
+    Private Sub cmbbutton9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbutton9.Click
+        If operasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "9"
+            operasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "9"
+        End If
+    End Sub
+
+    Private Sub cmbhapus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbhapus.Click
+        txtlayar.Text = ""
+        operasi = ""
+
+    End Sub
+
+    Private Sub cmbtambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbtambah.Click
+        operasi = "tambah"
+        Hasil = Val(txtlayar.Text)
+        operasiHitung = True
+    End Sub
+
+    Private Sub cmbsamadengan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbsamadengan.Click
+        Select Case operasi
+            Case "tambah"
+                Hasil = Hasil + Val(txtlayar .Text)
+            Case "kurang"
+                Hasil = Hasil - Val(txtlayar.Text)
+            Case "kali"
+                Hasil = Hasil * Val(txtlayar.Text)
+            Case "pangkat"
+                Hasil = Hasil ^ 2
+            Case "bagi"
+                Hasil = Hasil / Val(txtlayar.Text)
+        End Select
+        txtlayar.Text = Hasil
+        operasiHitung = True
+    End Sub
+
+    Private Sub cmbkurang_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbkurang.Click
+        operasi = "kurang"
+        Hasil = Val(txtlayar.Text)
+        operasiHitung = True
+    End Sub
+
+    Private Sub calculator_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub txtlayar_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtlayar.TextChanged
+
+    End Sub
+
+    Private Sub cmbkali_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbkali.Click
+        operasi = "kali"
+        Hasil = Val(txtlayar.Text)
+        operasiHitung = True
+    End Sub
+
+    Private Sub cmbpangkat_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbpangkat.Click
+        operasi = "pangkat"
+        Hasil = Val(txtlayar.Text)
+        operasiHitung = True
+    End Sub
+
+    Private Sub cmbbagi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbbagi.Click
+        operasi = "bagi"
+        Hasil = Val(txtlayar.Text)
+        operasiHitung = True
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        End
+    End Sub
+End Class
+
+
+
+
+
+
